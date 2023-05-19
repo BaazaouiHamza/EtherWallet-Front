1792be64df78c57b76cf0dc558ef36277ea49135
BaazaouiHamza <hamza.baazaoui@esprit.tn>
2023-05-19 16:28:52 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AppBar, Button, Typography, Toolbar, IconButton, Box } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import Home from './Components/Home';
+import SignUp from './Components/SignUp/SignUp';
+import SignIn from './Components/SignIn/SignIn';
 
-function App() {
+const HeaderLayout = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            EtherWallet+
+          </Typography>
+          <Button color='inherit' href='/signin'>Login</Button>
+          <Button color='inherit' href='/signup'>Register</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+
+const App = () => {
+  return (
+    <Router>
+      <HeaderLayout />
+      <Box sx={{ mt: 8, ml: 'auto', mr: 'auto' }}>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/signup' element={<SignUp />}></Route>
+          <Route path='/signin' element={<SignIn />}></Route>
+        </Routes >
+      </Box>
+    </Router>
   );
 }
 
