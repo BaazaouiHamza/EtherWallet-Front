5653c501d3d903f24fe896aaaeb2fcbeab7b0fc4
BaazaouiHamza <hamza.baazaoui@esprit.tn>
2023-05-19 17:02:31 +0100

    add signup form

--- src/Components/SignUp/SignUp.tsx
+++ src/Components/SignUp/SignUp.tsx
@@ -1,7 +1,26 @@
+import { Box, Button, Card, TextField, Typography } from '@mui/material'
 import React from 'react'
 
 const SignUp = () => {
-    return <h1> SignUp</h1>
+    return (
+        <Card sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            margin: "50px",
+            padding: "50px"
+        }}>
+            <form >
+                <Typography variant='h2' sx={{ marginBottom: '1rem', marginTop: '2rem' }} >Sign Up</Typography>
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+                    <TextField label="Email" type="email" fullWidth />
+                    <TextField label="Password" type="password" fullWidth />
+                    <TextField label="Repeat Password" type='password' />
+                    <Button variant='contained' type='submit' color='primary' >Register</Button>
+                </Box>
+            </form>
+        </Card>
+    )
 }
 
 export default SignUp
