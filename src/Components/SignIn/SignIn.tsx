4fe37ea421757d5685421f50433801ac1f23cc9c
BaazaouiHamza <hamza.baazaoui@esprit.tn>
2023-05-20 23:46:48 +0100

    add signin form

--- src/Components/SignIn/SignIn.tsx
+++ src/Components/SignIn/SignIn.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { Box, Button, Card, TextField, Typography } from '@mui/material'
 
 const SignIn = () => {
-    return <h1> SignIn</h1>
+    return (
+        <Card sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            margin: "50px",
+            padding: "50px"
+        }}>
+            <form >
+                <Typography variant='h2' sx={{ marginBottom: '1rem', marginTop: '2rem' }} >Sign In</Typography>
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+                    <TextField label="Email" type="email" fullWidth />
+                    <TextField label="Password" type="password" fullWidth />
+                    <Button color='inherit' href='/signup'>Register</Button>
+                </Box>
+            </form>
+        </Card>
+    )
 }
 
 export default SignIn
